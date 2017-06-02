6e388ccc5ae083fb9d7bfa1927def43b8b3eb7b0
slee <shonrii.github@gmail.com>
2017-06-02 08:03:02 -0700

    pull out toolbar from sidenav container

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,15 +3,7 @@ import { Component } from '@angular/core';
 // TODO: seamless transition between static sidebar into fully extended material sidebar
 @Component({
   template:  `
-    <div fxLayout="row" fxLayoutAlign="space-between center">
-      <div fxFlex="5%">
-        <!--<app-side-bar *ngIf="!sidenav.opened"></app-side-bar>-->
-        <app-side-bar></app-side-bar>
-      </div>
-      <div fxFlex="94%">
-        <router-outlet></router-outlet>
-      </div>
-    </div>
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['./home.component.scss']
 })

b2306819eff6c7c22d0918da6e59fad930c2b45b
slee <shonrii.github@gmail.com>
2017-06-23 14:30:00 -0700

    fix layout state assignment

--- src/app/store/reducers/layout.ts
+++ src/app/store/reducers/layout.ts
@@ -11,14 +11,14 @@ const initialState: State = {
 export function reducer(state = initialState, action: layout.Actions): State {
   switch (action.type) {
     case layout.CLOSE_SIDENAV:
-      return {
+      return Object.assign({}, state, {
         showSidenav: false
-      };
+      });
 
     case layout.OPEN_SIDENAV:
-      return {
+      return Object.assign({}, state, {
         showSidenav: true
-      };
+      });
 
     default:
       return state;

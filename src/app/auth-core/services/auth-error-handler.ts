d21b1eeb262d9dc46c73a59fe490ca49ccd35c23
slee <shonrii.github@gmail.com>
2017-07-05 09:20:50 -0700

    use router store for auth error handler

--- src/app/auth-core/services/auth-error-handler.ts
+++ src/app/auth-core/services/auth-error-handler.ts
@@ -1,15 +1,19 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
-import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { go } from '@ngrx/router-store';
+import * as fromRoot from '../../store';
 
 @Injectable()
 export class AuthErrorHandler implements ErrorHandler {
 
-  constructor(private injector: Injector) { }
+  constructor(
+    private injector: Injector,
+    private store: Store<fromRoot.State>
+  ) { }
 
   handleError(error) {
     if (error.rejection.status === 401 || error.rejection.status === 403) {
-      const router = this.injector.get(Router);
-      router.navigate(['/login']);
+      this.store.dispatch(go('/login'));
     }
 
     throw error;
